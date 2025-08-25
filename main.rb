9f412fc9ab4f1fe14e49c6c020871d3d90af7c84
Daniel Bengl <daniel.bengl@renuo.ch>
2025-08-25 19:55:14 +0200

    Implement basic raymarching with sdf

--- main.rb
+++ main.rb
@@ -61,15 +61,12 @@ class Scene
   attr_accessor :spheres
   def initialize
     @spheres = []
+    @spheres << Sphere.new(Vec3.new(0, 0, 5), 2)
+    @spheres << Sphere.new(Vec3.new(-4, 0, 8), 2)
   end
 
   def sdf(point)
-    min_distance = Float::INFINITY
-    @spheres.each do |sphere|
-      distance = sphere.sdf(point)
-      min_distance = [min_distance, distance].min
-    end
-    min_distance
+    @spheres.map { |sphere| sphere.sdf(point) }.min
   end
 end
 
@@ -80,18 +77,16 @@ class Ray
     @direction = direction
   end
 
-  def march(scene, step_size, max_distance)
-    max_steps = max_distance / step_size
-    t = 0.0
-    for i in 0..max_steps
-      point = origin + direction * t
+  def march(scene, max_distance)
+    traversed = 0.0
+    while traversed < max_distance
+      point = origin + direction * traversed
       distance = scene.sdf(point)
-      if distance < 0.001
-        return t
-      end
-      t += distance
+      return traversed if distance < 0.01
+
+      traversed += distance
     end
-    return max_distance
+    max_distance
   end
 end
 
@@ -102,19 +97,28 @@ class Renderer
     @width = width
     @height = height
     @depth = depth
+
+    @origin = Vec3.new(0, 0, 0)
+    @fov_percent = 1.5
   end
 
   def render(scene)
+    aspect_ratio = @width.to_f / @height.to_f
+    far_plane_width = @depth * @fov_percent * aspect_ratio
+    far_plane_height = @depth * @fov_percent
+
     rows = []
     @height.times do |y|
       row = []
       @width.times do |x|
-        ray = Ray.new(Vec3.new(0, 0, 0), Vec3.new(x, y, 1))
-        ray.direction = ray.direction.normalize
-        distance = ray.march(scene, 0.01, @depth)
-        if distance < @depth
-          row << distance
-        end
+        far_plane_point = Vec3.new(
+          far_plane_width * (x / @width.to_f) - far_plane_width / 2,
+          far_plane_height * (y / @height.to_f) - far_plane_height / 2,
+          @depth
+        )
+        direction = far_plane_point - @origin
+        ray = Ray.new(@origin, direction.normalize)
+        row << ray.march(scene, @depth)
       end
       rows << row
     end
@@ -126,25 +130,24 @@ end
 class Main
   def initialize
     @scene = Scene.new
-    @scene.spheres << Sphere.new(Vec3.new(0, 0, 0), 1)
     @renderer = Renderer.new(32, 32, 10)
   end
 
   def distance_to_color(distance)
-    normalized_distance = rand(0..1)
+    normalized_distance = distance / @renderer.depth
     case normalized_distance
     when 0.95..1.0
-      "\e[47m█\e[0m"  # White full block
+      "\e[47m█\e[0m"
     when 0.8...0.95
-      "\e[47m▓\e[0m"  # Heavy shade
+      "\e[47m▓\e[0m"
     when 0.6...0.8
-      "\e[47m▒\e[0m"  # Medium shade
+      "\e[47m▒\e[0m"
     when 0.4...0.6
-      "\e[47m░\e[0m"  # Light shade
+      "\e[47m░\e[0m"
     when 0.2...0.4
-      "\e[47m \e[0m"  # White space
+      "\e[47m \e[0m"
     else
-      "\e[40m \e[0m"  # Black space
+      "\e[40m \e[0m"
     end * 2
   end
 
